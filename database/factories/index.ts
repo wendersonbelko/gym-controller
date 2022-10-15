fb453f1610c3356d312cbb2db941c71f1170228b
wenderson belko <contato@wendersonbelko.com>
2022-10-15 15:33:46 -0300

    user factory

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,1 +1,18 @@
-// import Factory from '@ioc:Adonis/Lucid/Factory'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+import { UserAccountRole, UserAccountStatus } from 'App/contracts/UserContract'
+import User from 'App/Models/User'
+import { DateTime } from 'luxon'
+
+export const userFactory = (status: UserAccountStatus, role: UserAccountRole) =>
+  Factory.define(User, ({ faker }) => {
+    return {
+      name: faker.name.fullName(),
+      email: faker.internet.email(),
+      cpf: '88774116002',
+      birthday: new Date() as unknown as DateTime,
+      password: faker.internet.password(),
+      photo: faker.internet.avatar(),
+      accountStatus: status,
+      role: role,
+    }
+  }).build()
