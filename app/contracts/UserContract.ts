b2ca18785e5458809137b75138b2682bef6161f6
wenderson belko <contato@wendersonbelko.com>
2022-10-15 02:10:30 -0300

    user contract

---
+++ app/contracts/UserContract.ts
@@ -0,0 +1,12 @@
+export enum UserAccountRole {
+  USER = 'USER',
+  TEACHER = 'TEACHER',
+  GYM = 'GYM',
+}
+
+export enum UserAccountStatus {
+  PENDING = 'PENDING',
+  ACTIVE = 'ACTIVE',
+  SUSPENDED = 'SUSPENDED',
+  DELETED = 'DELETED',
+}
