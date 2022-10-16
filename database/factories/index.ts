511d5677974e8a9eac48e5b632c4a244c64f7b3f
wenderson belko <contato@wendersonbelko.com>
2022-10-16 02:23:29 -0300

    update user factory

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -9,7 +9,7 @@ export const userFactory = (status: UserAccountStatus, role: UserAccountRole) =>
       name: faker.name.fullName(),
       email: faker.internet.email(),
       cpf: '88774116002',
-      birthday: new Date() as unknown as DateTime,
+      birthday: '2000-04-07T03:58:28.025Z' as unknown as DateTime,
       password: faker.internet.password(),
       photo: faker.internet.avatar(),
       accountStatus: status,
