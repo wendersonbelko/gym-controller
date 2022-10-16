3b1b6d9ec3a31c390a2650a389c0929626af9f31
wenderson belko <contato@wendersonbelko.com>
2022-10-16 14:08:02 -0300

    configure swagger

--- start/routes.ts
+++ start/routes.ts
@@ -1,1 +1,7 @@
+import Route from '@ioc:Adonis/Core/Route'
 import './routes/user'
+
+Route.get('/', async ({ view }) => {
+  const specUrl = 'docs'
+  return view.render('swagger', { specUrl })
+})
